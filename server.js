200656fb0f518603e997945e18291a79d1585edd
TariqAli93 <tariqali9300@gmail.com>
2024-07-20 20:17:25 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(express.json());
+
+app.post("/send-email", async (req, res) => {
+  const { name, phone, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "tariqali9300@gmail.com",
+      pass: "jwob lkzd qxbm zizz"
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: "tariqali9300@gmail.com",
+    subject: `New message from ${name} - ${email}`,
+    html: `
+      <p>Name: ${name}</p>
+      <p>Phone: ${phone}</p>
+      <p>Email: ${email}</p>
+      <p><strong>Message:</strong></p>
+        <p>${message}</p>
+      `
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return res.status(200).send("Email sent");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Error sending email");
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
